8c68d1798afc9f1384b6469995ae57db6e8691af
Amber Jain <jainanuamber@gmail.com>
2022-08-05 15:16:28 +0530

    updated

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,6 +10,47 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Flutter Provider Example App"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [const Text("You have pushed the button this many times:")],
+        ),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            key: const Key('decrement_floatingActionButton'),
+            tooltip: 'Decrement',
+            child: const Icon(Icons.remove),
+            onPressed: () {},
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          FloatingActionButton(
+              key: const Key('reset_floatingActionButton'),
+              tooltip: 'Reset',
+              child: const Icon(Icons.exposure_zero),
+              onPressed: () {}),
+          const SizedBox(
+            width: 10,
+          ),
+          FloatingActionButton(
+              key: const Key('increment_floatingActionButton'),
+              tooltip: 'Increment',
+              child: const Icon(Icons.add),
+              onPressed: () {}),
+          const SizedBox(
+            width: 10,
+          ),
+        ],
+      ),
+    );
   }
 }
