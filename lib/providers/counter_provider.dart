04a54d831910479cac3fbe6ddafc0670378dea2a
Amber Jain <jainanuamber@gmail.com>
2022-08-05 15:23:27 +0530

    counter_provider added

---
+++ lib/providers/counter_provider.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Counter with ChangeNotifier {
+  int _count = 0;
+  int get count => _count;
+
+  void increment() {
+    _count++;
+  }
+
+  void decrement() {
+    _count--;
+  }
+
+  void reset() {
+    _count = 0;
+  }
+}
